430deb46399ab56a3f96c5dbfee4d4f05e0dd8fe
Stenius <stenius@gmail.com>
2011-01-12 21:10:11 -0600

    removed some debugging information and some light refactoring

--- openTbl.py
+++ openTbl.py
@@ -1,26 +1,16 @@
 from struct import unpack,unpack_from
 fid = open('lab_1.tbl')
 ind = 128
-#get the number of rows
-header = ''
-#for x in range(0,10):
-#    header += unpack('c',fid.read(1))[0]
-#header = unpack('<10c',fid.read(10))
-#print 'the header is',header
 fid.seek(ind)
 a = unpack('c',fid.read(1))[0]
-print 'a is ',a
 while a>chr(12):
     ind += 1
     a = unpack('c',fid.read(1))[0]
-    print 'a is ',a
 ind += 1
 a = unpack('c',fid.read(1))[0]
-print 'a is ',a
 while a>chr(12):
     ind += 1
     a = unpack('c',fid.read(1))[0]
-    print 'a is ',a
 
 d_count = 1
 fcon =1
@@ -29,14 +19,11 @@ while fcon == 1:
     fid.seek(ind)
     marker1 = unpack('B',fid.read(1))[0]
     marker2 = unpack('B',fid.read(1))[0]
-    #print marker1,marker2
     if marker1 == 0 and marker2 == 75:
-        print 'YES'
         fcon = 0
     else:
         d_count += 1
         ind += 12
-print 'd_count is',d_count
 ind = 122
 data = []
 labels = []
@@ -63,7 +50,6 @@ while fcon == 1:
             chann += a
             ind += 1
             a = unpack('c',fid.read(1))[0]
-    #    print chann,units
         labels.append((chann + ',' + units))
         ind += 17
         #start aquiring data entries
@@ -72,18 +58,21 @@ while fcon == 1:
             aux = unpack('d',fid.read(8))[0]
             if abs(aux) < 10**-150:
                 aux=0
-            #print aux
             out.append(aux)
             ind += 12
         out.reverse()
         output.append(out)
-        #data = flipud(out)
         ind +=7
     else:
         fcon = 0
 out= map(lambda *row: list(row), *output )
-print labels
-for x in out:
-    for y in x:
-        print y,'\t',
-    print '\n'
+
+if __name__ == '__main__':
+    for label in labels:
+        print label,'\t',
+    print ''
+    for x in out:
+        for y in x:
+            print y,'\t',
+        print '\n',
+ 
